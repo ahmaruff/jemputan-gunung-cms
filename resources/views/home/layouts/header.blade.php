c399fc8222c03264941e1a21f006d2f47e363423
ahmaruff <ahmadmaruf2701@gmail.com>
2023-02-24 02:39:51 +0700

    FIX header & footer homepage

--- resources/views/home/layouts/header.blade.php
+++ resources/views/home/layouts/header.blade.php
@@ -1,15 +1,38 @@
-<nav class="d-flex flex-wrap justify-content-center py-3 px-5">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <img src="/assets/images/logo.png" alt="Jemputan Gunung" class="img-fluid me-3" width="40">
-        <span class="fs-4">Jemputan Gunung</span>
-    </a>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark px-4">
+    <div class="container-fluid py-1">
+        <a class="navbar-brand" href="/"><img src="/assets/images/logo.png" class="img-fluid" width="45" alt="Logo"><span class="fw-bold ms-2 me-4">Jemputan Gunung</span></a>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHome" aria-controls="navbarHome" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active rounded" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-    </ul>
+        <div class="collapse navbar-collapse" id="navbarHome">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/trip">Trip</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/kontak">Kontak</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/tentang">Tentang Kami</a>
+                </li>
+                {{-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li> --}}
+            </ul>
+            <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="btn btn-outline-secondary" href="/bantuan">Bantuan</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
-    

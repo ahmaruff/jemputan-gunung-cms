e01bd4bc12c5a899428e410818987755529c8835
ahmaruff <ahmadmaruf2701@gmail.com>
2023-03-07 15:16:18 +0700

    change function name

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function dashboard()
     {
         $user = Auth::user();
         $data = [

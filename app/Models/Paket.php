08d3226c9cb547a8ea246b9ef48805817b39bb90
ahmaruff <ahmadmaruf2701@gmail.com>
2023-03-19 14:05:44 +0700

    fix(app) add fillable attribute in Paket models and change column

--- app/Models/Paket.php
+++ app/Models/Paket.php
@@ -5,10 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use function PHPSTORM_META\map;
+
 class Paket extends Model
 {
     use HasFactory;
-    
+    protected $fillable =  [
+        'judul',
+        'thumbnail',
+        'penjemputan',
+        'deskripsi',
+        'durasi',
+        'rencana_perjalanan',
+        'harga',
+        'minimal_pax',
+    ];
+
     public function destinasi()
     {
         return $this->belongsToMany(Destinasi::class);

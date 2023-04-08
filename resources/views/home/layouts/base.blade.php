00426d2d15de6c6edcd6db9348de353880bd8776
ahmaruff <ahmadmaruf2701@gmail.com>
2023-04-09 04:35:26 +0700

    enable bootstrapp cdn

--- resources/views/home/layouts/base.blade.php
+++ resources/views/home/layouts/base.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous"> --}}
-    <link rel="stylesheet" href="/assets/libs/bootstrap-5.2.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    {{-- <link rel="stylesheet" href="/assets/libs/bootstrap-5.2.2/css/bootstrap.min.css"> --}}
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     @yield('head')
@@ -27,7 +27,7 @@
 
     @include('home.layouts.footer')
     
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script> --}}
-    <script src="/assets/libs/bootstrap-5.2.2/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    {{-- <script src="/assets/libs/bootstrap-5.2.2/js/bootstrap.bundle.min.js"></script> --}}
 </body>
 </html>
